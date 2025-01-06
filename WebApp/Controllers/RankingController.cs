cec9e1e7af988091bcfcc4f7e3405fb5a70205b9
Jakub Putowski <jakub.putowski@microsoft.wsei.edu.pl>
2025-01-06 13:02:45 +0100

    strocniowanie w detailsach

--- WebApp/Controllers/RankingController.cs
+++ WebApp/Controllers/RankingController.cs
@@ -13,27 +13,30 @@ public class RankingController : Controller
         _context = context;
     }
 
-    public IActionResult Details(int universityId, int rankingSystemId)
+    public async Task<IActionResult> Details(int universityId, int rankingSystemId, int pageIndex = 1, int pageSize = 20)
     {
         // Pobieramy szczegóły rankingu dla uniwersytetu
-        var details = _context.UniversityRankingYears
-            .Where(ury => ury.UniversityId == universityId && 
+        var source = _context.UniversityRankingYears
+            .Where(ury => ury.UniversityId == universityId &&
                           ury.RankingCriteria.RankingSystemId == rankingSystemId)
             .Select(ury => new RankingDetailsViewModel
             {
                 Year = ury.Year ?? 0, // Ustaw 0, jeśli Year jest null
                 CriteriaName = ury.RankingCriteria.CriteriaName ?? "Unknown", // Ustaw "Unknown", jeśli CriteriaName jest null
                 Score = ury.Score ?? 0 // Ustaw 0, jeśli Score jest null
-            })
-            .ToList();
+            });
+
+        var paginatedList = await PaginatedList<RankingDetailsViewModel>.CreateAsync(source, pageIndex, pageSize);
 
         // Pobieramy dane uniwersytetu i systemu rankingowego
         var university = _context.Universities.Find(universityId);
         var rankingSystem = _context.RankingSystems.Find(rankingSystemId);
 
+        ViewBag.UniversityId = universityId;
+        ViewBag.RankingSystemId = rankingSystemId;
         ViewBag.UniversityName = university.UniversityName;
         ViewBag.RankingSystemName = rankingSystem.SystemName;
 
-        return View(details);
+        return View(paginatedList);
     }
 }
